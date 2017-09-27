954ef7753eb66c654492e119274c6a8a34e008bb
RED-GATE\clare.walsh <clare.walsh@red-gate.com>
2017-09-27 15:44:15 +0100

    Add a handler function for the onSubmit event of the form

--- app2/src/App.js
+++ app2/src/App.js
@@ -10,6 +10,23 @@ class App extends Component {
     this.state = {
       products: data.products
     };
+    this.addHandler = this.addHandler.bind(this);
+  }
+
+  addHandler(e) {
+    var name = e.target[0].value;
+    var description = e.target[1].value;
+    var product = {
+      name: name,
+      description: description
+    };
+    var products = this.state.products.concat(product);
+
+    this.setState({
+      products: products
+    });
+
+    e.preventDefault();
   }
 
   render() {
@@ -18,11 +35,11 @@ class App extends Component {
         <h2>Kata 2- Add and remove objects</h2>
       </div>
       <div className='add-product'>
-        <form>
-          <label for="name">Product Name</label>
+        <form onSubmit={this.addHandler}>
+          <label htmlFor="name">Product Name</label>
           <input id="name" type="text" />
 
-          <label for="description">Description</label>
+          <label htmlFor="description">Description</label>
           <input id="description" type="text" />
 
           <button type="submit">Add Product</button>
